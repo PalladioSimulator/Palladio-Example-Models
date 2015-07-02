dd453c5724eaf13e0668fac6e8d795d6faa59f47
AnneKoziolek <anne.koziolek@kit.edu>
2015-07-02 10:54:27 +0000

    script for PCM 4.0 migration (not yet well tested, feel free to adapt)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Examples@28093 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ pcm_3.5_to_4.0_migration.bat
@@ -0,0 +1,66 @@
+rem the replacement commands are executed for all PCM files in the current folder. #
+rem Possibly not all metamodel changes have been accounted for, so some may have to be done manually. 
+
+for /f %%a IN ('dir /b *.repository') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f %%a IN ('dir /b *.usagemodel') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f %%a IN ('dir /b *.allocation') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f %%a IN ('dir /b *.system') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f %%a IN ('dir /b *.resourceenvironment') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f %%a IN ('dir /b *.cost') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+)
+
+for /f "delims=" %%a IN ('dir /b *.designdecision') do (
+  sed -i "s/sdq.ipd.uka.de\/StochasticExpressions\/.../sdq.ipd.uka.de\/StochasticExpressions\/2.2/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel/palladiosimulator.org\/PalladioComponentModel/g" "%%a"
+  sed -i "s/sdq.ipd.uka.de\/DesignDecision\/..0/sdq.ipd.uka.de\/DesignDecision\/3.0\" xmlns:specific=\"http:\/\/sdq.ipd.uka.de\/DesignDecision\/Specific\/3.0/g" "%%a"
+  sed -i "s/designdecision:AssembledComponentDegree/specific:AssembledComponentDegree/g" "%%a"
+  sed -i "s/designdecision:AllocationDegree/specific:AllocationDegree/g" "%%a"
+  sed -i "s/designdecision:ClassAsReferenceDegree/specific:ClassAsReferenceDegree/g" "%%a"
+  sed -i "s/designdecision:ContinuousRangeDegree/specific:ContinuousRangeDegree/g" "%%a"
+  sed -i "s/designdecision:DiscreteRangeDegree/specific:DiscreteRangeDegree/g" "%%a"
+  sed -i "s/designdecision:OrderedIntegerDegree/specific:OrderedIntegerDegree/g" "%%a"
+  sed -i "s/designdecision:ContinuousComponentParamDegree/specific:ContinuousComponentParamDegree/g" "%%a"
+  sed -i "s/designdecision:ContinuousProcessingRateDegree/specific:ContinuousProcessingRateDegree/g" "%%a"
+  sed -i "s/designdecision:DiscreteComponentParamDegree/specific:DiscreteComponentParamDegree/g" "%%a"
+  sed -i "s/designdecision:DiscreteProcessingRateDegree/specific:DiscreteProcessingRateDegree/g" "%%a"
+  sed -i "s/designdecision:FeatureGroupDegree/specific:FeatureGroupDegree/g" "%%a"
+  sed -i "s/designdecision:FeatureSubset/specific:FeatureSubset/g" "%%a"
+  sed -i "s/designdecision:NumberOfCoresAsListDegree/specific:NumberOfCoresAsListDegree/g" "%%a"
+  sed -i "s/designdecision:NumberOfCoresAsRangeDegree/specific:NumberOfCoresAsRangeDegree/g" "%%a"
+  sed -i "s/designdecision:OptionalFeatureDegree/specific:OptionalFeatureDegree/g" "%%a"
+  sed -i "s/designdecision:ResourceContainerReplicationDegree/specific:ResourceContainerReplicationDegree/g" "%%a"
+  sed -i "s/designdecision:ResourceContainerReplicationDegreeWithComponentChange/specific:ResourceContainerReplicationDegreeWithComponentChange/g" "%%a"
+  sed -i "s/designdecision:ResourceSelectionDegree/specific:ResourceSelectionDegree/g" "%%a"
+  sed -i "s/designdecision:StringComponentParamDegree/specific:StringComponentParamDegree/g" "%%a"
+  sed -i "s/designdecision:CapacityDegree/specific:CapacityDegree/g" "%%a"
+  sed -i "s/designdecision:MonitoringDegree/specific:MonitoringDegree/g" "%%a"
+
+)
+
+@echo ********
+@echo Try whether your models are valid now. If not, you may have to change more things manually. Check out https://sdqweb.ipd.kit.edu/wiki/PCM_Model_Migration and https://sdqweb.ipd.kit.edu/wiki/PCM_Changelog
+
+
+goto:eof
